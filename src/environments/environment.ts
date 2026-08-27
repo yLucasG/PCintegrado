ae9856d09c51608d93cf88419580ff46789680f5
yLucasG <67508767+yLucasG@users.noreply.github.com>
2026-08-27 10:38:54 -0300

    feat: add Supabase environment configuration

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://lyeoxvvhwdhwrscnvwhl.supabase.co',
+  supabaseAnonKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5ZW94dnZod2Rod3JzY252d2hsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4MjA1MTMsImV4cCI6MjEwMzM5NjUxM30.5UmTJGS53A80R1MVBDc5bigGWjjxyvitPdL8VO_89zM',
+};
